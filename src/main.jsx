f39e2463dccc00f651e957027042bfeab04c1229
V-a-m-c <korlavamsi74@gmail.com>
2025-10-21 16:47:40 +0530

    updated browser routers

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './styles/global.scss';
-
+import { BrowserRouter } from 'react-router-dom';
 ReactDOM.createRoot(document.getElementById('app')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename='/DhiAsphire-AI-webpage/'>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
 );
